903c66812c89f32ff932b792cfef2b57e8951cff
matiushariman <matius.hariman@gmail.com>
2022-12-10 21:35:13 +0700

    initial commit

---
+++ apps/shell/module-federation.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+/**
+ * @type {import('@nrwl/devkit').ModuleFederationConfig}
+ **/
+const moduleFederationConfig = {
+  name: 'shell',
+  remotes: [],
+};
+
+module.exports = moduleFederationConfig;
---
+++ apps/shell/src/app/app.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import NxWelcome from './nx-welcome';
+import { Link, Route, Routes } from 'react-router-dom';
+
+export function App() {
+  return (
+    <React.Suspense fallback={null}>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<NxWelcome title="shell" />} />
+      </Routes>
+    </React.Suspense>
+  );
+}
+
+export default App;
