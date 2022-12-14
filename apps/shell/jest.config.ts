2ddce5bde5340f6838f60ec88736ff8533fb0aad
matiushariman <matius.hariman@gmail.com>
2022-12-14 09:21:03 +0700

    [ci] add integration ci for home app and shell app

--- apps/shell/jest.config.ts
+++ apps/shell/jest.config.ts
@@ -8,4 +8,5 @@ export default {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/shell',
+  coverageReporters: ['json', ['text', { skipFull: true }]],
 };
