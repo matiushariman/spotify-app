706af8fee6665ced683eb75ce47204867192ecad
matiushariman <matius.hariman@gmail.com>
2023-01-17 12:56:38 +0700

    init react app setup

---
+++ apps/react-spotify-app/src/app/App.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './App.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="react-spotify-app" />
+      <div />
+    </>
+  );
+}
+
+export default App;
---
+++ lint-staged.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  '*.{js,jsx,ts,tsx}': 'eslint --cache --fix',
+  '*.{js,jsx,ts,tsx,css,md,mdx}': 'prettier --write',
+};
