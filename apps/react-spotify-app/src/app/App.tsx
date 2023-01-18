2cde0ff77c6fb87d0b6582b7f7f110c0c9b85464
matiushariman <matius.hariman@gmail.com>
2023-01-18 17:09:48 +0700

    add authRoutes

--- apps/react-spotify-app/src/app/App.tsx
+++ apps/react-spotify-app/src/app/App.tsx
@@ -1,22 +1,12 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Login, Callback, Dashboard } from '@react-spotify/auth-feature';
+import { routes as authRoutes } from '@react-spotify/auth-feature';
 import { useSessionStore } from '@react-spotify/shared-stores';
 import { useMemo } from 'react';
 
 export function App() {
   const isAuthenticated = useSessionStore((state) => state.isAuthenticated);
   const router = useMemo(
-    () =>
-      createBrowserRouter([
-        {
-          path: '/',
-          element: isAuthenticated ? <Dashboard /> : <Login />,
-        },
-        {
-          path: '/callback',
-          element: <Callback />,
-        },
-      ]),
+    () => createBrowserRouter([...authRoutes]),
     [isAuthenticated]
   );
 
--- libs/auth/feature/src/index.ts
+++ libs/auth/feature/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/routes/routes';
 export * from './lib/Dashboard/Dashboard';
 export * from './lib/Callback/Callback';
 export * from './lib/Login/Login';
