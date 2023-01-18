e64c6b6d5d2f742ef6817808de6b2d768648caa8
matiushariman <matius.hariman@gmail.com>
2023-01-18 12:17:05 +0700

    add Avatar component

--- libs/auth/components/src/index.ts
+++ libs/auth/components/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/Avatar/Avatar';
 export * from './lib/AppBar/AppBar';
 export * from './lib/SpotifyLogo/SpotifyLogo';
 export * from './lib/SideNavigation/SideNavigation';
---
+++ libs/auth/components/src/lib/Avatar/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Avatar } from './Avatar';
+
+const Story: ComponentMeta<typeof Avatar> = {
+  component: Avatar,
+  title: 'Avatar',
+};
+export default Story;
+
+const Template: ComponentStory<typeof Avatar> = (args) => <Avatar {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  src: 'https://i.pravatar.cc/300',
+  username: 'User',
+};
+
+export const Fallback = Template.bind({});
+Fallback.args = {};
