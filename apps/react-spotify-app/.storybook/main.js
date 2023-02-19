a8c121a1a99af022b6dd6b9c84aa09f9af4ed9e2
matiushariman <matius.hariman@gmail.com>
2023-02-19 14:19:57 +0800

    add react-docgen-typescript config

--- apps/react-spotify-app/.storybook/main.js
+++ apps/react-spotify-app/.storybook/main.js
@@ -17,6 +17,16 @@ module.exports = {
     ...(rootMain.addons ?? []),
   ],
   staticDirs: [...rootMain.staticDirs],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
   async viteFinal(config, { configType }) {
     return mergeConfig(config, {
       plugins: [
